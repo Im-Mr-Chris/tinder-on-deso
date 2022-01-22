9ec86dd50697a5954651314e54119f2abf9adbf9
Mr. Chris <im-mr-chris@protonmail.ch>
2022-01-22 04:34:03 +0000

    update

--- App.js
+++ App.js
@@ -8,10 +8,9 @@ import StackNavigator from "./StackNavigator";
 
 export default function App() {
   return (
-    <NavigationContainer ref={navigationRef}>
-      <AuthProvider>
-        <StackNavigator />
-      </AuthProvider>
-    </NavigationContainer>
+    </View style={tw("flex-1 justify-center items-center")}>
+      <Test>Hello DeSo!</Test>
+      <Button title='Click me' />
+    </View>
   );
 }
